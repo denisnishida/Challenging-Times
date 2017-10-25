b691080f957c87b2f0135b2490a16cad97bd7782
denisnishida <denis.nishida@gmail.com>
2017-10-25 18:36:55 +0000

    Created table data with Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+30.times do
+  Character.create(name: Faker::Zelda.character,
+                   location: Faker::Zelda.location)
+end
