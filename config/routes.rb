1717546b7441412a19d933a2deb075329715e6e8
denisnishida <denis.nishida@gmail.com>
2017-10-25 18:00:35 +0000

    Created lizards action and added the 3 links - Finished part 2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'pages', to: 'pages#home', as: 'pages'
   get 'pages/home', to: 'pages#home', as: 'pages_home'
   get 'pages/about_us', to: 'pages#about', as: 'about_page'
+  get 'pages/lizards/:number', to: 'pages#lizards', as: 'lizards', number: /\d+/
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
