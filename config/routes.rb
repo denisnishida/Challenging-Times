6ba6c5567a3a4e86c1d4921602abd214d2a5705a
denisnishida <denis.nishida@gmail.com>
2017-10-25 17:32:18 +0000

    Renamed about URL - Finished part 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get 'pages', to: 'pages#home', as: 'pages'
   get 'pages/home', to: 'pages#home', as: 'pages_home'
-  get 'pages/about', to: 'pages#about', as: 'about_page'
+  get 'pages/about_us', to: 'pages#about', as: 'about_page'
   root to: 'pages#home'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
